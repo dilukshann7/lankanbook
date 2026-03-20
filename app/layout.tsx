26ecc5fe1b6ea21e10e580fc8733a5fafcb4bd75
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2026-03-20 16:16:45 +0530

    Refactor: Remove custom font class definitions and imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,10 @@
-import { Inter, Playfair_Display, DM_Mono } from "next/font/google"
+import { Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils"
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
-
-const fontMono = DM_Mono({
-  weight: ["400", "500"],
-  subsets: ["latin"],
-  variable: "--font-mono-dm",
-})
-
-const fontPlayfair = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-playfair",
-})
 
 export default function RootLayout({
   children,
@@ -26,13 +15,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn(
-        "antialiased",
-        inter.variable,
-        fontMono.variable,
-        fontPlayfair.variable,
-        "font-sans"
-      )}
+      className={cn("antialiased", inter.variable, "font-sans")}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
